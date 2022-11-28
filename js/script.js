debb1466b24ef97b8277ff05157c81fe4d5491d2
tomms25 <tommasobaccano@gmail.com>
2022-11-28 16:52:06 +0100

    prova inserimento title e text tramite vue/htmls

--- js/script.js
+++ js/script.js
@@ -51,8 +51,26 @@ const { createApp } = Vue;
                         'img/04.webp',
                         'img/05.webp',
                     ],
-                    // Seconda e terza proprietà
                     
+                    // Seconda proprietà
+                    title: [
+                        'Marvel\'s Spiderman Miles Morale',
+                        'Ratchet & Clank: Rift Apart',
+                        'Fortnite',
+                        'Stray',
+                        "Marvel's Avengers",
+                    ]
+
+                    // Terza proprietà
+
+                    text:[
+                        'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                        'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                        "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                        'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                        'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+
+                    ]
                 },
             }
 
